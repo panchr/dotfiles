407d0a36da7fe66354c1f9a5ce11d6e4af31a656
Rushy Panchal <panchal.rushy@gmail.com>
2019-10-07 21:03:45 -0700

    Ensure packages to build Python are installed on Linux

--- bootstrap/install-packages.sh
+++ bootstrap/install-packages.sh
@@ -6,4 +6,35 @@ $PACKAGE_MANAGER install \
 		 tmux \
 		 emacs \
 		 htop \
-		 tree
+		 tree \
+		 libpq-dev
+
+case `uname` in
+    Darwin)
+	# do nothing
+    ;;
+    Linux)
+	$PACKAGE_MANAGER install \
+			 make \
+			 build-essential \
+			 libssl-dev \
+			 zlib1g-dev \
+			 libbz2-dev \
+			 libreadline-dev \
+			 libsqlite3-dev \
+			 wget \
+			 curl \
+			 llvm \
+			 libncurses5-dev \
+			 libncursesw5-dev \
+			 xz-utils \
+			 tk-dev \
+			 libffi-dev \
+			 liblzma-dev \
+			 python-openssl \
+			 git
+    ;;
+    FreeBSD)
+	# do nothing
+    ;;
+esac
