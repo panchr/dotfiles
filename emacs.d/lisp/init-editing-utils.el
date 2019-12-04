236dc741be38b6869a8638236e7c405765a271a6
Rushy Panchal <panchal.rushy@gmail.com>
2019-12-04 14:29:38 -0800

    [emacs] Add simpler shortcut for comment-line

--- emacs.d/lisp/init-editing-utils.el
+++ emacs.d/lisp/init-editing-utils.el
@@ -175,6 +175,7 @@
 ;;----------------------------------------------------------------------------
 (global-set-key (kbd "C-.") 'set-mark-command)
 (global-set-key (kbd "C-x C-.") 'pop-global-mark)
+(global-set-key (kbd "C-x C-_") 'comment-line)
 
 (when (maybe-require-package 'avy)
   (global-set-key (kbd "C-;") 'avy-goto-char-timer))
