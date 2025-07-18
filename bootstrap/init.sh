87514a72c02befc4b36f4e5672d76bd7789349ce
Rushy Panchal <panchal.rushy@gmail.com>
2025-07-18 08:10:50 -0400

    [bootstrap] Force-link config

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -7,11 +7,11 @@ CONFIG_DIR="$SCRIPT_DIR/.."
 
 # Dotfiles are symlinked so they are kept up-to-date when the repository is
 # pulled.
-ln -s "$CONFIG_DIR/.sh_functions" ~/.sh_functions
-ln -s "$CONFIG_DIR/.zshrc" ~/.zshrc
-ln -s "$CONFIG_DIR/.tmux.conf" ~/.tmux.conf
-ln -s "$CONFIG_DIR/.emacs.d" ~/.emacs.d
+ln -s -f "$CONFIG_DIR/.sh_functions" ~/.sh_functions
+ln -s -f "$CONFIG_DIR/.zshrc" ~/.zshrc
+ln -s -f "$CONFIG_DIR/.tmux.conf" ~/.tmux.conf
+ln -s -f "$CONFIG_DIR/.emacs.d" ~/.emacs.d
 
 # Claude settings.
 mkdir -p ~/.claude
-ln -s "$CONFIG_DIR/.claude/settings.json" ~/.claude/settings.json
+ln -s -f "$CONFIG_DIR/.claude/settings.json" ~/.claude/settings.json
