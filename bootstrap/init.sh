a7597edb52e03774a8f5826506194a451bfd5ed9
Rushy Panchal <panchal.rushy@gmail.com>
2025-07-18 08:54:44 -0400

    [bootstrap] Fix emacs linking

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -3,14 +3,14 @@
 set -ex
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-CONFIG_DIR="$SCRIPT_DIR/.."
+CONFIG_DIR=$(realpath "$SCRIPT_DIR/..")
 
 # Dotfiles are symlinked so they are kept up-to-date when the repository is
 # pulled.
 ln -s -f "$CONFIG_DIR/.sh_functions" ~/.sh_functions
 ln -s -f "$CONFIG_DIR/.zshrc" ~/.zshrc
 ln -s -f "$CONFIG_DIR/.tmux.conf" ~/.tmux.conf
-ln -s -f "$CONFIG_DIR/.emacs.d" ~/.emacs.d
+ln -s -f "$CONFIG_DIR/emacs.d" ~/.emacs.d
 
 # Claude settings.
 mkdir -p ~/.claude
