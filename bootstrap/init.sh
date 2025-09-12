a2541641d52aa7fd76b808237f6a4e4d74757bbc
Rushy Panchal <panchal.rushy@gmail.com>
2025-09-12 09:15:11 -0600

    reorganize dotfiles

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -2,20 +2,20 @@
 
 set -ex
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 CONFIG_DIR=$(realpath "$SCRIPT_DIR/..")
 
 # Dotfiles are symlinked so they are kept up-to-date when the repository is
 # pulled.
-ln -s -f "$CONFIG_DIR/.sh_functions" ~/.sh_functions
-ln -s -f "$CONFIG_DIR/.zshrc" ~/.zshrc
-ln -s -f "$CONFIG_DIR/.tmux.conf" ~/.tmux.conf
+ln -s -f "$CONFIG_DIR/zsh/sh_functions" ~/.sh_functions
+ln -s -f "$CONFIG_DIR/zsh/zshrc" ~/.zshrc
+ln -s -f "$CONFIG_DIR/tmux/tmux.conf" ~/.tmux.conf
 
 # Claude settings.
 mkdir -p ~/.claude
 mkdir -p ~/.claude/commands
-ln -s -f "$CONFIG_DIR/.claude/settings.json" ~/.claude/settings.json
-ln -s -f "$CONFIG_DIR/.claude/CLAUDE.md" ~/.claude/CLAUDE.md
-ln -s -f "$CONFIG_DIR/.claude/commands/address-review.md" ~/.claude/commands/address-review.md
+ln -s -f "$CONFIG_DIR/claude/settings.json" ~/.claude/settings.json
+ln -s -f "$CONFIG_DIR/claude/CLAUDE.md" ~/.claude/CLAUDE.md
+ln -s -f "$CONFIG_DIR/claude/commands/address-comments.md" ~/.claude/commands/address-comments.md
 
 $SCRIPT_DIR/init-doom.sh
