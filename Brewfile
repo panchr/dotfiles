f69ccf523d78bf5ec4fb51ba2986305383e2bd69
Rushy Panchal <panchal.rushy@gmail.com>
2025-09-12 19:55:17 -0600

    bootstrap: switch to a Brewfile

---
+++ Brewfile
@@ -0,0 +1,60 @@
+# Terminal
+brew "zsh"
+brew "tmux"
+
+# Terminal utilities
+brew "tree"
+brew "vnstat"
+brew "telnet"
+brew "watch"
+brew "grep"
+brew "htop"
+brew "ripgrep"
+brew "rsync"
+brew "the_silver_searcher"
+brew "yq"
+brew "jq"
+brew "noti"
+brew "fzf"
+brew "fd"
+
+# Git
+brew "gh"
+brew "git"
+
+# Emacs
+tap "d12frosted/emacs-plus"
+brew "d12frosted/emacs-plus/emacs-plus@30"
+
+# Python
+brew "pyenv"
+brew "python@3.11"
+brew "openssl"
+brew "readline"
+brew "sqlite3"
+brew "xz"
+brew "zlib"
+
+# Languages
+brew "bazelisk"
+brew "buildifier"
+brew "clang-format"
+brew "go"
+brew "rust"
+brew "shellcheck"
+brew "shfmt"
+brew "terraform"
+brew "tflint"
+brew "llvm"
+
+# Misc
+brew "awscli"
+brew "binwalk"
+brew "findutils"
+brew "coreutils"
+brew "gnutls"
+brew "gnupg"
+brew "ffmpeg"
+brew "imagemagick"
+brew "openssh"
+brew "pinentry-mac"
--- bootstrap/install-packages.sh
+++ bootstrap/install-packages.sh
@@ -1,113 +1,75 @@
 #!/bin/bash
 
-DIR="$( dirname -- "$BASH_SOURCE"; )";
+set -ex
+
+DIR="$(dirname -- "$BASH_SOURCE")"
 
 PACKAGE_MANAGER='brew'
 REPOSITORY_MANAGER='brew tap'
 
-case `uname` in
-    Darwin)
+case $(uname) in
+Darwin)
 	# Ensure that brew is installed.
 	if ! command -v "$PACKAGE_MANAGER" &>/dev/null; then
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	fi
-    ;;
-    Linux)
+	;;
+Linux)
 	PACKAGE_MANAGER='sudo apt-get -y'
 	REPOSITORY_MANAGER='sudo add-apt-repository -y'
-    ;;
-    FreeBSD)
+	;;
+FreeBSD)
 	# do nothing
-    ;;
+	;;
 esac
 
-$PACKAGE_MANAGER install \
-		 tmux \
-		 htop \
-		 tree
-
-case `uname` in
-    Darwin)
-		# General utilities
-		$PACKAGE_MANAGER install \
-			the_silver_searcher \
-			ripgrep\
-			go \
-			bazelisk \
-			zsh \
-			tmux \
-			grep \
-			openssh \
-			git \
-			tree \
-			gnupg \
-			findutils \
-			coreutils \
-			awscli \
-			vnstat \
-			watch \
-			telnet \
-			pinentry-mac \
-			noti \
-			claude-code \
-			gh \
-			fd
-
-		# Emacs.
-		$REPOSITORY_MANAGER d12frosted/emacs-plus
-		$PACKAGE_MANAGER install emacs-plus
-
-		# Python.
-		$PACKAGE_MANAGER install \
-						 pyenv \
-						 python@3.10 \
-						 openssl \
-						 readline \
-						 sqlite3 \
-						 xz \
-						 zlib
-
-    ;;
-    Linux)
+case $(uname) in
+Darwin)
+	$PACKAGE_MANAGER bundle install
+	;;
+Linux)
 	$REPOSITORY_MANAGER ppa:git-core/ppa
 	$REPOSITORY_MANAGER ppa:kelleyk/emacs
 	$PACKAGE_MANAGER update
 
 	$PACKAGE_MANAGER install \
-			 make \
-			 build-essential \
-			 libssl-dev \
-			 zlib1g-dev \
-			 libbz2-dev \
-			 libreadline-dev \
-			 libsqlite3-dev \
-			 libpq-dev \
-			 wget \
-			 curl \
-			 llvm \
-			 libncurses5-dev \
-			 libncursesw5-dev \
-			 xz-utils \
-			 tk-dev \
-			 libffi-dev \
-			 liblzma-dev \
-			 python-openssl \
-			 git \
-			 emacs26 \
-			 silversearcher-ag
-    ;;
-    FreeBSD)
+		make \
+		build-essential \
+		libssl-dev \
+		zlib1g-dev \
+		libbz2-dev \
+		libreadline-dev \
+		libsqlite3-dev \
+		libpq-dev \
+		wget \
+		curl \
+		llvm \
+		libncurses5-dev \
+		libncursesw5-dev \
+		xz-utils \
+		tk-dev \
+		libffi-dev \
+		liblzma-dev \
+		python-openssl \
+		git \
+		silversearcher-ag
+
+	bash "$DIR/install-emacs30-ubuntu.sh"
+	;;
+FreeBSD)
 	# do nothing
-    ;;
+	;;
 esac
 
 # Other installations
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-pyenv install --skip-existing 3.10.3
-pyenv local 3.10.3
-pip3 install --upgrade \
-    pip \
-    pipenv
+if [ ! -d ~/.pyenv ]; then
+	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+	pyenv install --skip-existing 3.10.3
+	pyenv local 3.10.3
+	pip3 install --upgrade \
+		pip \
+		pipenv
+fi
 
 # Install language servers.
 go install golang.org/x/tools/gopls@latest
