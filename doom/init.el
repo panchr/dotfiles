6994409787a63db3db67eac054d9e54412b90596
Rushy Panchal <panchal.rushy@gmail.com>
2025-10-15 20:54:17 +0100

    emacs: enable mode-line

--- doom/init.el
+++ doom/init.el
@@ -19,27 +19,27 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       company             ; the ultimate code completion backend
        ;;(corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       vertico             ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
-       doom              ; what makes DOOM look the way it does
+       doom                ; what makes DOOM look the way it does
        ;; doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; Emoji support
-       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
-       ;; minimap           ; show a map of the code on the side
-       ;;(modeline +light)   ; snazzy, Atom-inspired modeline, plus API
+       ;; minimap          ; show a map of the code on the side
+       (modeline +light)   ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
-       ;;1Mneotree         ; a project drawer, like NERDTree for vim
-       ;;ophints             ; highlight the region an operation acts on
+       ;;neotree           ; a project drawer, like NERDTree for vim
+       ophints             ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        smooth-scroll       ; So smooth you won't believe it's not butter
        ;; tabs             ; a tab bar for Emacs
