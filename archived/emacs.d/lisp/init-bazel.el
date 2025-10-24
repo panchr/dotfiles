189c77345c76bc557c962da4bf08ba6f410e9693
Rushy Panchal <panchal.rushy@gmail.com>
2025-10-24 22:44:15 +0100

    archived: delete all archived config

--- archived/emacs.d/lisp/init-bazel.el
+++
@@ -1,8 +0,0 @@
-;;; init-bazel.el --- Bazel editing -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-(require-package 'bazel)
-(setq bazel-mode-buildifier-before-save t)
-(provide 'init-bazel)
-;;; init-bazel.el ends here
--- archived/emacs.d/lisp/init-java.el
+++
@@ -1,9 +0,0 @@
-;;; init-java.el --- Java support -*- lexical-binding: t -*-
-
-;;; Code:
-
-(use-package kotlin-mode
-  :ensure t)
-
-(provide 'init-java)
-;;; init-java.el ends here
